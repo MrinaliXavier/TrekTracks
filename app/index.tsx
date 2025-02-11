a906c9edf9b4c0b61a085413dfa51ad03a29d271
MrinaliXavier <xmrinali@gmail.com>
2025-02-11 21:41:30 +0530

    frontEnd_basic

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
 import { Text, View } from "react-native";
+import Login from './../components/Login'
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        alignItems:"center",
+        justifyContent:"center"
+        
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <Login/>
+    
     </View>
   );
 }
